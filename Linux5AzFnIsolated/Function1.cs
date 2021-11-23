a6cf0162e8564b2f1eae2d16c4a9e90fe5b4437f
Simon Crouch <iamontheinternet@yahoo.com>
2021-11-23 17:04:35 +0000

    add PW into function

--- Linux5AzFnIsolated/Function1.cs
+++ Linux5AzFnIsolated/Function1.cs
@@ -1,25 +1,36 @@
 using System.Collections.Generic;
 using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
+using Microsoft.Playwright;
 
 namespace Linux5AzFnIsolated
 {
     public static class Function1
     {
         [Function("Function1")]
-        public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req,
-            FunctionContext executionContext)
+        public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req, FunctionContext executionContext)
         {
             var logger = executionContext.GetLogger("Function1");
             logger.LogInformation("C# HTTP trigger function processed a request.");
 
             var response = req.CreateResponse(HttpStatusCode.OK);
-            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            // response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome to Azure Functions!");
+            // response.WriteString("Welcome to Azure Functions!");
 
+            // return response;
+
+            response.Headers.Add("Content-Type", "image/png");
+
+            using var playwright = await Playwright.CreateAsync();
+            await using var browser = await playwright.Chromium.LaunchAsync();
+            var page = await browser.NewPageAsync();
+            await page.GotoAsync("https://playwright.dev/dotnet");
+            response.WriteBytes(await page.ScreenshotAsync());
             return response;
         }
     }
