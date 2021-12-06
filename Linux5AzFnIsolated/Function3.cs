6d913894cefadc0c68637b025ccf1f5bdd525ff3
Hirofumi Horikawa <hori.kawa.shima.tool+github@gmail.com>
2021-12-06 19:36:02 +0900

    Add `mount` function

---
+++ Linux5AzFnIsolated/Function3.cs
@@ -0,0 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Linux5AzFnIsolated
+{
+  public static class Function3
+  {
+    [Function("Function3")]
+    public static async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req,
+        FunctionContext executionContext)
+    {
+      ProcessStartInfo startInfo = new ProcessStartInfo
+      {
+        FileName = "sh",
+        ArgumentList = {"-c", "mount"},
+        RedirectStandardInput = true,
+        RedirectStandardOutput = true,
+        RedirectStandardError = true,
+        UseShellExecute = false
+      };
+      Process process = Process.Start(startInfo);
+      var query = System.Web.HttpUtility.ParseQueryString(req.Url.Query);
+      string output = process.StandardOutput.ReadToEnd();
+      process.WaitForExit();
+      var response = req.CreateResponse(HttpStatusCode.OK);
+      response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+      response.WriteString(output);
+      return response;
+    }
+  }
+}
